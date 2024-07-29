6715e54c9336d2724e99acb1ab1fd72b8fd85d8b
Ruan Swanepoel <ruane.swanepoel@gmail.com>
2024-07-30 00:29:13 +1000

    feat: Make the table list display prettier

--- src/disp.rs
+++ src/disp.rs
@@ -6,25 +6,24 @@ fn print_table(content: &Vec<Vec<String>>) {
         return;
     }
 
-    let cols = content[0].len();
+    let headers = vec![
+        String::from("Id"),
+        String::from("Ip Address"),
+        String::from("Hostname"),
+    ];
 
-    // Get column widths
-    let mut col_widths: Vec<usize> = vec![0; cols];
+    // Calculate column widths
+    let mut col_widths: Vec<usize> = vec![2, 10, 8];
     content.iter().for_each(|row| {
         row.iter().enumerate().for_each(|(i, cell)| {
             col_widths[i] = std::cmp::max(col_widths[i], cell.chars().count());
         });
     });
 
-    // Print headers?
-    print_separator(&col_widths);
-    let headers = vec![
-        String::from("Id"),
-        String::from("Ip Address"),
-        String::from("Hostname"),
-    ];
-    print_row(&headers, &col_widths);
+    print_table_top_separator(&col_widths);
 
+    // Print headers
+    print_row(&headers, &col_widths);
     print_separator(&col_widths);
 
     // Print rows
@@ -32,7 +31,7 @@ fn print_table(content: &Vec<Vec<String>>) {
         print_row(&row, &col_widths);
     });
 
-    print_separator(&col_widths);
+    print_table_bottom_separator(&col_widths);
 }
 
 fn print_row(row: &Vec<String>, col_widths: &Vec<usize>) {
@@ -41,17 +40,35 @@ fn print_row(row: &Vec<String>, col_widths: &Vec<usize>) {
         .enumerate()
         .map(|(i, val)| format!("{:<w$}", val, w = col_widths[i]))
         .collect::<Vec<String>>()
-        .join(" | ");
-    println!("| {} |", inner);
+        .join(" │ ");
+    println!("│ {} │", inner);
+}
+
+fn print_table_top_separator(col_widths: &Vec<usize>) {
+    let inner = col_widths
+        .iter()
+        .map(|width| format!("─{:─<w$}─", "", w = width))
+        .collect::<Vec<String>>()
+        .join("┬");
+    println!("┌{}┐", inner);
+}
+
+fn print_table_bottom_separator(col_widths: &Vec<usize>) {
+    let inner = col_widths
+        .iter()
+        .map(|width| format!("─{:─<w$}─", "", w = width))
+        .collect::<Vec<String>>()
+        .join("┴");
+    println!("└{}┘", inner);
 }
 
 fn print_separator(col_widths: &Vec<usize>) {
     let inner = col_widths
         .iter()
-        .map(|width| format!("-{:-<w$}-", "", w = width))
+        .map(|width| format!("─{:─<w$}─", "", w = width))
         .collect::<Vec<String>>()
-        .join("+");
-    println!("+{}+", inner);
+        .join("┼");
+    println!("├{}┤", inner);
 }
 
 pub fn list_hosts() {
